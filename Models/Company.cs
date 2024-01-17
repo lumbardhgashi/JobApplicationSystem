5b5439f27bbe1a6a8aaa95367106faba031d0df6
TigranGashi <93322726+TigranGashi@users.noreply.github.com>
2024-01-17 21:58:00 +0100

    Feature_05_CompanyModelAndEntity "Added Company.cs at Model and CompanyEntity.cs at Entities "

---
+++ Models/Company.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Models
+{
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+        [Required]
+        public string CompanyName { get; set; }
+        [Required]
+        public string Sektori { get; set; }
+        [Required]
+        public string Adresa { get; set; }
+        [Required]
+        public int NumriTelefonit { get; set; }
+    }
+}
