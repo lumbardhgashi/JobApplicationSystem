3733ce6d63e72262872f4704c4adb198713fe16d
Xon1 <93322958+Xon1@users.noreply.github.com>
2024-01-17 21:40:25 +0100

    Feature_01_SkillSetModelAndEntity  "SkillSet.cs and SkillSetEntity.cs "

---
+++ Models/SkillSet.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Models
+{
+    public class SkillSet
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ApplicantId { get; set; }
+        [Required]
+        public string Skills { get; set; }
+        [Required]
+        public string Pointer { get; set; }
+    }
+}
