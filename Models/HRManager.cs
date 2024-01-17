40dac96bffa8bfd8787831cace5946264a10a8a7
TigranGashi <93322726+TigranGashi@users.noreply.github.com>
2024-01-17 22:41:07 +0100

    Feature_06_HRMenagerModelAndEntity "Added HRMenager.cs at Model and HRMenagerEntity.cs at Entities "

---
+++ Models/HRManager.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Models
+{
+    public class HRManager
+    {
+        [Key]
+        public int HRManagerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int CompanyId { get; set; }
+        [Required]
+        public string Department { get; set; }
+    }
+}
