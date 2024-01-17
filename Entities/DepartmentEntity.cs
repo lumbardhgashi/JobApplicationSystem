39ee9b8cf996f053767fd415e8a71035b4f8d56e
TigranGashi <93322726+TigranGashi@users.noreply.github.com>
2024-01-17 22:21:24 +0100

    Feature_04_DepartmentModelAndEntity "Added Department.cs at Model and DepartmentEntity.cs at Entities "

--- Entities/DepartmentEntity.cs
+++ Entities/DepartmentEntity.cs
@@ -1,6 +1,18 @@
-﻿namespace JobApplicationSystem.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Entities
 {
     public class DepartmentEntity
     {
+        [Key]
+        public int DepartmentId { get; set; }
+        [Required]
+        public int CompanyId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int NumriPunonjesve { get; set; }
+        [Required]
+        public string DrejtoriDepartamentit { get; set; }
     }
 }
