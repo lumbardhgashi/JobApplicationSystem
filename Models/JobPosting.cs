f254b5eddc3726fb78e6fb0f66b71b84979872c7
lumbardhgashi <lg52753@ubt-uni.net>
2024-01-18 15:44:46 +0100

    Feature_09_JobPostingModelAndEntity

---
+++ Models/JobPosting.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Models
+{
+    public class JobPosting
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int HrManagerId { get; set; }
+        [Required]
+        public int DepartmentId { get; set; }
+        [Required]
+        public string Tittle { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        public string Requriments { get; set; }
+    }
+}
