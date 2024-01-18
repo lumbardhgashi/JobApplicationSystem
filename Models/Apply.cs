11b2d56e69b0548db036e90708d3d9a1f0926213
lumbardhgashi <lg52753@ubt-uni.net>
2024-01-18 15:36:41 +0100

    Feature_08_ApplyModelAndEntity

---
+++ Models/Apply.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Models
+{
+    public class Apply
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int applicantId { get; set; }
+        [Required]
+        public int JobPostId { get; set; }
+    }
+}
