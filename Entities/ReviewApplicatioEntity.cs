4f65a00289350c56ce8b6e9f8c0bc533e93f1782
lumbardhgashi <lg52753@ubt-uni.net>
2024-01-18 15:49:53 +0100

    Feature_10_ReviewApplicationAndEntity

---
+++ Entities/ReviewApplicatioEntity.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Entities
+{
+    public class ReviewApplicatioEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int HrManagerId { get; set; }
+        [Required]
+        public int ApplyId { get; set; }
+        [Required]
+        public string PointOfReview { get; set; }
+    }
+}
