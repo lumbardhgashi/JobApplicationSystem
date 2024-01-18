67aad3107bae05c143c2a5baf5a8ec747c0f3d4a
lumbardhgashi <lg52753@ubt-uni.net>
2024-01-18 15:55:05 +0100

    Feature_11_ApplicationStatusModelAndEntiy

---
+++ Models/ApplicationStatus.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobApplicationSystem.Models
+{
+    public class ApplicationStatus
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int ReviewApplicationId { get; set; }
+        [Required]
+        public string Status { get; set; }
+        public string StatusDescription { get; set; }
+    }
+}
